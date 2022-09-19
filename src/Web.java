500a1ad2213075ce22123518a18526e65c43e5ac
Adrimr7 <adriandecerio@gmail.com>
2022-09-19 23:00:38 +0200

    Métodos de clase Web actualizados.

--- src/Web.java
+++ src/Web.java
@@ -1,2 +1,31 @@
-public class Web {
+import java.util.ArrayList;
+
+public class Web
+{
+    private String nombre;
+    private Integer id;
+    private ArrayList<Integer> listaRelaciones;
+
+    public Web(String pNombre, Integer pId)
+    {
+        this.id = pId;
+        this.nombre = pNombre;
+    }
+    public Integer getId()
+    {
+        return (this.id);
+    }
+    public String getNombre()
+    {
+        return (this.nombre);
+    }
+    public ArrayList<Integer> getLista()
+    {
+        return (this.listaRelaciones);
+    }
+    public void editarLista()
+    {
+
+    }
 }
+
