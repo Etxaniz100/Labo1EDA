7410166dbd91cd19ce03b670dff3d9a54f2076ca
Etxaniz100 <79299525+Etxaniz100@users.noreply.github.com>
2022-09-19 23:56:27 +0200

    Actualizacion de la lectura de ficheros de texto

--- src/main.java
+++ src/main.java
@@ -1,6 +1,8 @@
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class main {
@@ -18,6 +20,8 @@ public class main {
 
     // ------ Metodos ------
     public static void main(String args[]) {
+        Path path = Paths.get("");
+        main.getMiMain().cargarLista( path.toAbsolutePath().toString() + "\\src\\index-2022-2023");
     //Metodo main
     }
 
@@ -32,19 +36,28 @@ public class main {
     }
 
     public void cargarLista(String nomF){
+
+        int cont = 0; //Borrar, es para que no compruebe los 5000000
+
         try{
             Scanner entrada = new Scanner(new FileReader(nomF));
             String linea;
-            while (entrada.hasNext()) {
+            while (entrada.hasNext() && cont < 5) { //Borrar, es para que no compruebe los 5000000
                 linea = entrada.nextLine();
+                String datos[] = linea.split(":+");
+                System.out.println(datos[0]);
+                System.out.println(datos[1]);
+                cont += 1; //Borrar, es para que no compruebe los 5000000
 
-
+                ListaWebs.getMiListaWebs().anadirWeb(new Web(datos[1],Integer.valueOf(datos[0])));
 
 
             }
             entrada.close();
         }
         catch(IOException e) {e.printStackTrace();}
+
+
     }
 
 }
